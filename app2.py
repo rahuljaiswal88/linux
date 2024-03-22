f853929e564a880278ebfc56da2f2a75efd8109c
rahuljaiswal88 <137074587+rahuljaiswal88@users.noreply.github.com>
2024-03-22 16:32:54 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,9 @@
+from flask import Flask
 
+app= Flask(__name__)
+
+@app.route('/')
+def hello_home():
+    return '<p> You have reached home page </p> <img src="https://www.dictionary.com/e/wp-content/uploads/2018/04/Mojo-Jojo.jpg" />'
+
+app.run(host="0.0.0.0", port=6000)
