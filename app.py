b8f15b2129461b16c0fe685a5d4980210ced4d7a
rahuljaiswal88 <137074587+rahuljaiswal88@users.noreply.github.com>
2024-03-22 16:27:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app= Flask(__name__)
 
 @app.route('/')
 def hello_home():
-    return '<p> You have reached home page </p> <img src="https://www.nokia.com/sites/default/files/styles/scale_720_not_convert_webp/public/2023-02/nokia-refreshed-logo-1_1.jpg" />'
+    return '<p> You have reached home page </p> <img src="https://www.nokia.com/sites/default/files/styles/scale_720_not_convert_webp/public/2023-02/nokia-refreshed-logo-1_1.jpg"/>'
 
-app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=6000)
